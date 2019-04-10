7e3521cb971c3632f5ec5a32c9671e8da499b681
ishanps2@illinois.edu <ishu@ps11>
2019-04-10 17:36:14 -0500

    NFC reader blockchain server code

---
+++ nfc_block/contract.py
@@ -0,0 +1,30 @@
+contract_source_code = '''
+pragma solidity ^0.4.21;
+
+contract Greeter {
+    event Greet(
+        bool greeting
+    );
+    bool public greeting;
+
+    function Greeter() public {
+        greeting = false;
+    }
+
+    function toggleGreeting() public {
+        greeting = !greeting;
+        emit Greet(greeting);
+    }
+
+    function greet() view public returns (bool) {
+        return greeting;
+    }
+}
+'''
+
+def get_contract(w3):
+    #return None
+     return w3.eth.contract(
+         address='0x99E9AC696419383c837eDf50656B4AaAd0387985',
+         abi=[{'constant': False, 'inputs': [], 'name': 'toggleGreeting', 'outputs': [], 'payable': False, 'stateMutability': 'nonpayable', 'type': 'function'}, {'constant': True, 'inputs': [], 'name': 'greet', 'outputs': [{'name': '', 'type': 'bool'}], 'payable': False, 'stateMutability': 'view', 'type': 'function'}, {'constant': True, 'inputs': [], 'name': 'greeting', 'outputs': [{'name': '','type': 'bool'}], 'payable': False, 'stateMutability': 'view', 'type': 'function'}, {'inputs': [], 'payable': False, 'stateMutability': 'nonpayable', 'type': 'constructor'}, {'anonymous': False, 'inputs': [{'indexed': False, 'name': 'greeting', 'type': 'bool'}], 'name': 'Greet', 'type': 'event'}]
+     )
