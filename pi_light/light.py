ef689086d8e512c5e0ade976a8db7c0630f1b093
ishanps2@illinois.edu <ishu@ps11>
2019-04-27 13:59:44 -0500

    Added time logging to light.py

--- pi_light/light.py
+++ pi_light/light.py
@@ -1,7 +1,11 @@
+import sys
+sys.path.insert(0, './../ethereum/')
 import RPi.GPIO as GPIO
 import time
 import pika
+from latency import openTimeLogFile, logTime, closeFile
 
+logFile = openTimeLogFile('light_log.txt')
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(23,GPIO.OUT)
@@ -12,6 +16,7 @@ channel = connection.channel()
 channel.queue_declare(queue='blk_to_gateway')
 
 def callback(ch, method, properties, body):
+    logTime(logFile,"receivedMQTTForLight")
     print(" [x] Received %r" % body)
     GPIO.output(23,GPIO.HIGH)
     time.sleep(1)
@@ -23,3 +28,4 @@ channel.basic_consume(queue='blk_to_gateway',
                       on_message_callback=callback)
 print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
+closeFile(logFile)
